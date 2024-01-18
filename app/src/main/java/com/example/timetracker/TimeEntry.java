541a31e347be78054e3b60e3c5bd0a5b37259e86
Lorenz Müller <muellerlorenz64@gmail.com>
2024-01-18 11:29:24 +0100

    create TimeEntry calss

---
+++ app/src/main/java/com/example/timetracker/TimeEntry.java
@@ -0,0 +1,27 @@
+package com.example.timetracker;
+
+public class TimeEntry {
+
+    private long startTime;
+    private long endTime;
+
+    public void setEndTime(long endTime) {
+        this.endTime = endTime;
+    }
+
+    public void setStartTime(long startTime) {
+        this.startTime = startTime;
+    }
+
+    public long getStartTime() {
+        return startTime;
+    }
+
+    public long getEndTime() {
+        return endTime;
+    }
+
+    public long getDuration() {
+        return endTime - startTime;
+    }
+}
