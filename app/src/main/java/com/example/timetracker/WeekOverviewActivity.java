afd444d19c57a819dfcd6617b86ead8f88b33e20
Lorenz Müller <muellerlorenz64@gmail.com>
2024-01-18 11:19:45 +0100

    create weekoverview activity

---
+++ app/src/main/java/com/example/timetracker/WeekOverviewActivity.java
@@ -0,0 +1,28 @@
+package com.example.timetracker;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WeekOverviewActivity extends AppCompatActivity {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_weekoverview);
+            Button backHomeBtn = findViewById(R.id.back);
+
+            backHomeBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(WeekOverviewActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            });
+
+
+
+        }
+}
