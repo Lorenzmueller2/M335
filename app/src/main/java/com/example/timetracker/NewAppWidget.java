c5bc463e64f5081bf12e5c325c085d0a2d30fd79
Lorenz Müller <muellerlorenz64@gmail.com>
2024-01-19 13:51:38 +0100

    update NewAppWidget

--- app/src/main/java/com/example/timetracker/NewAppWidget.java
+++ app/src/main/java/com/example/timetracker/NewAppWidget.java
@@ -6,9 +6,6 @@ import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.widget.RemoteViews;
 
-/**
- * Implementation of App Widget functionality.
- */
 public class NewAppWidget extends AppWidgetProvider {
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
